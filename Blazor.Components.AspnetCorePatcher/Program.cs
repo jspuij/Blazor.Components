3dda9d64626730b0abd7f35336dca44c8f490b0e
Jan-Willem Spuij <jw@spuij.nl>
2020-01-29 10:36:19 +0100

    Add project files.

---
+++ Blazor.Components.AspnetCorePatcher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Blazor.Components.AspnetCorePatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
